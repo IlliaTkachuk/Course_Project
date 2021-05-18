e974d67ee507f0b80462d6df0c9c7b52b15635a0
IlliaTkachuk <illiatkachuk2001@gmail.com>
2021-05-18 22:41:26 +0300

    Changed string "\n" issue

--- Course_Project/Course_Project.cpp
+++ Course_Project/Course_Project.cpp
@@ -31,48 +31,41 @@ public:
 
 class MenuService {
 private:
-	string menu1 = "1.Manage faculties\n2.Manage student groups";
-	string menu2 = "1.Create faculty\n2.Find all faculties\n3.Find faculty\n4.Update faculty\n5.Delete faculty";
-	string menu3 = "1.Find faculty by name\n2.Find faculty by id";
-	string menu4 = "1.Change faculty name\n2.Assign dean\n3.Add group\n4.Delete group";
-	string menu5 = "1.Create group\n2.Find all groups\n3.Find group\n4.Update group\n5.Delete group";
-	string menu6 = "1.Find group by name\n2.Find group by id";
-	string menu7 = "1.Change group name\n2.Assign headman\n3.Add student\n4.Delete student";
+	string menu1 = "1.Manage faculties\n2.Manage student groups\n";
+	string menu2 = "1.Create faculty\n2.Find all faculties\n3.Find faculty\n4.Update faculty\n5.Delete faculty\n";
+	string menu3 = "1.Find faculty by name\n2.Find faculty by id\n";
+	string menu4 = "1.Change faculty name\n2.Assign dean\n3.Add group\n4.Delete group\n";
+	string menu5 = "1.Create group\n2.Find all groups\n3.Find group\n4.Update group\n5.Delete group\n";
+	string menu6 = "1.Find group by name\n2.Find group by id\n";
+	string menu7 = "1.Change group name\n2.Assign headman\n3.Add student\n4.Delete student\n";
 	
 public:
 	void printMenu1() {
 		cout << menu1;
-		cout << "\n";
 	}
 
 	void printMenu2() {
 		cout << menu2;
-		cout << "\n";
 	}
 
 	void printMenu3() {
 		cout << menu3;
-		cout << "\n";
 	}
 
 	void printMenu4() {
 		cout << menu4;
-		cout << "\n";
 	}
 
 	void printMenu5() {
 		cout << menu5;
-		cout << "\n";
 	}
 
 	void printMenu6() {
 		cout << menu6;
-		cout << "\n";
 	}
 
 	void printMenu7() {
 		cout << menu7;
-		cout << "\n";
 	}
 };
 
