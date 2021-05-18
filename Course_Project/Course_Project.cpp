6ac77a25c34f578f5c01c564928f631b618759fa
IlliaTkachuk <illiatkachuk2001@gmail.com>
2021-05-18 20:39:15 +0300

    Created ConsoleService

--- Course_Project/Course_Project.cpp
+++ Course_Project/Course_Project.cpp
@@ -1,12 +1,65 @@
 ﻿#include <iostream>
 #include <string>
+#include <windows.h>
+
 using namespace std;
 
+class ConsoleService {
+
+public:
+	string inputString() {
+		string input;
+		cin >> input;
+		return input;
+	}
+
+	int inputInt() {
+		int input;
+		cin >> input;
+		return input;
+	}
+
+	void outputString(string input) {
+		cout << input + "\n";
+	}
+
+	void clearConsole() {
+		int seconds = 10;
+		cout << "\nClosing in: ";
+		while (seconds != 0) {
+			cout << seconds;
+			Sleep(1000);
+			seconds--;
+			cout << "\b";
+		}
+		system("cls");
+	}
+
+};
 
 int main() {
 
-	cout << "Hello World!\n";
+	ConsoleService service;
+	
+	
+	int intResult = service.inputInt();
+	string stringResult = service.inputString();
+	cout << "OutputString: ";
+	service.outputString(stringResult);
+
+	service.clearConsole();
 
 	system("pause");
 	return 0;
 }
+
+
+/*
+faculty crud
+	sorting
+
+	group crud
+		sorting
+		starosta
+		student
+*/
